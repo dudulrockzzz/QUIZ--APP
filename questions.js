1816848b79381c294be160560d6d6869e6085627
Saswat mohanty <87393034+dudulrockzzz@users.noreply.github.com>
2023-05-26 09:40:39 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,113 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: " What is the difference between the 'is' and '==' operators in Python? ? ",
+    answer: " 'is' checks for object identity while '==' checks for object equality.",
+    options: [
+      " 'is' checks for object identity while '==' checks for object equality.",
+      " '==' checks for object identity while 'is' checks for object equality",
+      "Both operators check for object identity",
+      "Both operators check for object equality"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is a generator in Python?",
+    answer: "A function that generates a sequence of values lazily",
+    options: [
+      "A function that generates random numbers",
+      "A function that returns an iterable object",
+      "A function that generates a sequence of values lazily",
+      "A function that generates a list of values"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What module would you use to build a GUI for a Python quiz app?",
+    answer: "tkinter",
+    options: [
+      "tkinter",
+      "pygame",
+      "flask",
+      "requests"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which data structure is best suited for storing quiz questions in a Python quiz app?",
+    answer: "dictionaries",
+    options: [
+      "lists",
+      "tuples",
+      "sets",
+      "dictionaries"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is the purpose of the init() method in a Python class?",
+    answer: "To initialize instance variables",
+    options: [
+      "To create an instance of the class",
+      "To initialize instance variables",
+      "To define class methods",
+      "To define class attributes"
+    ]
+  },
+  {
+    numb: 6,
+    question: "What is the purpose of the open() function in Python?",
+    answer: "All of the above",
+    options: [
+      "To create a new file",
+      "To read data from a file",
+      "To write data to a file",
+      "All of the above"
+    ]
+  },
+  {
+    numb: 7,
+    question: "What is the purpose of the format() method in Python?",
+    answer: "To format a string with placeholders",
+    options: [
+      "To convert a string to an integer",
+      "To join multiple strings together",
+      "To format a string with placeholders",
+      "To split a string into a list"
+    ]
+  },
+  {
+    numb: 8,
+    question: "What module can you use to generate random numbers in a Python quiz app?",
+    answer: "random",
+    options: [
+      "random",
+      "math",
+      "statistics",
+      "numpy"
+    ]
+  },
+  {
+    numb: 9,
+    question: "What is the purpose of the os module in Python?",
+    answer: "To interact with the operating system",
+    options: [
+      "To interact with the operating system",
+      "To perform mathematical operations",
+      "To handle exceptions",
+      "To create graphical user interfaces"
+    ]
+  },
+  {
+    numb: 10,
+    question: "What is the purpose of the pip package manager in Python?",
+    answer: "To install and manage Python packages",
+    options: [
+      "To install and manage Python packages",
+      "To create virtual environments",
+      "To debug Python code",
+      "To optimize Python code"
+    ]
+  },
+];
